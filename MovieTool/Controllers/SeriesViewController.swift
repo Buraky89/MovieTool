ad8bfdb16696e26e1277c5ab2db998b9c0419e85
buraky89 <yavuzalpb@itu.edu.tr>
2021-05-04 13:49:46 +0300

    TableViewController and TableView stuff

---
+++ MovieTool/Controllers/SeriesViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  MovieTool
+//
+//  Created by Burak Yavuzalp on 4.05.2021.
+//
+
+import UIKit
+import SDWebImage
+
+class SeriesViewController: UITableViewController {
+
+    var seriesManager = SeriesManager()
+    var seriesResult: SeriesResult?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        seriesManager.delegate = self
+        
+        seriesManager.getSeries()
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return seriesResult?.results.count ?? 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath)// as! CustomCell
+        
+        
+
+        cell.textLabel?.text = seriesResult?.results[indexPath.row].name
+        
+        var imgPath = "https://www.themoviedb.org/t/p/w440_and_h660_face/\(seriesResult?.results[indexPath.row].backdropPath ?? "")"
+        
+        cell.imageView?.sd_setImage(with: URL(string: imgPath), placeholderImage: UIImage(systemName: "questionmark"))
+
+        return cell
+    }
+    
+}
+
+extension SeriesViewController: MovieDelegate {
+    
+    func updateList(seriesResult: SeriesResult) {
+        self.seriesResult = seriesResult
+        tableView.reloadData()
+    }
+    
+}
--- MovieTool/Model/SeriesManager.swift
+++ MovieTool/Model/SeriesManager.swift
@@ -9,21 +9,17 @@ import Foundation
 import Alamofire
 
 class SeriesManager {
-    //var url = "https://api.themoviedb.org/3/discover/movie?api_key=408446855a7ec2113c33da1df8d69427&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
-    var url = "https://api.themoviedb.org/3/discover/tv?api_key=408446855a7ec2113c33da1df8d69427&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate"
-    
-    
     var delegate: MovieDelegate?
     
-    func getUrl(parent: String) -> String {
-        return "\(url)\(parent)"
+    func getUrl() -> String {
+        return K.SeriesUrl.replacingOccurrences(of: "{apiKey}", with: K.ApiKey)
     }
     
     func getSeries()
     {
 
         
-        NetworkManager.instance.fetch(HTTPMethod.get, url: url, requestModel: nil, model: SeriesResult.self) { response in
+        NetworkManager.instance.fetch(HTTPMethod.get, url: getUrl(), requestModel: nil, model: SeriesResult.self) { response in
               
               switch(response)
               {
